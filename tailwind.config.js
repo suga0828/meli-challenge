2fe2c463616936d66ff837b6025a229d83a1e60b
Alexander Sandoval <alexander.sf0828@gmail.com>
2022-03-01 00:55:05 -0500

    feat: add custom tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: theme =>({
+        ...theme('colors'),
+        'primary': '#FFE600',
+        'gray-3': '#333333',
+        'gray-6': '#666666',
+        'gray-9': '#999999',
+        'gray-e': '#eeeeee',
+      }),
+    },
   },
   plugins: [],
 }
